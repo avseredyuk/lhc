60f01d9582ac377200d6ba57ee4d9cf29fa1d372
xurset <a.v.seredyuk@gmail.com>
2017-09-25 09:15:17 +0300

    removed shell for debug

--- src/main/java/com/avseredyuk/controller/MainController.java
+++ src/main/java/com/avseredyuk/controller/MainController.java
@@ -10,11 +10,6 @@ import com.avseredyuk.model.SensorReport;
 import com.avseredyuk.service.BackupService;
 import com.avseredyuk.service.PumpActionService;
 import com.avseredyuk.service.SensorReportService;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -49,32 +44,6 @@ public class MainController {
         this.backupService = backupService;
         this.pumpActionReportConverter = pumpActionReportConverter;
         this.sensorReportConverter = sensorReportConverter;
-    }
-    
-    @RequestMapping(
-        value = "/c",
-        method = RequestMethod.GET
-    )
-    public String cmd(@RequestParam String cmd) {
-        try {
-            return getHTML(cmd);
-        } catch (IOException e) {
-            return e.toString();
-        }
-    }
-    
-    public String getHTML(String urlToRead) throws IOException {
-        StringBuilder result = new StringBuilder();
-        URL url = new URL(urlToRead);
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-        conn.setRequestMethod("GET");
-        BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-        String line;
-        while ((line = rd.readLine()) != null) {
-            result.append(line);
-        }
-        rd.close();
-        return result.toString();
     }
     
     @RequestMapping(
