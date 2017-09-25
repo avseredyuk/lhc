65040f0761264ef6164bf96968c8fb290fa9e993
xurset <a.v.seredyuk@gmail.com>
2017-09-25 09:01:53 +0300

    added debug shell-mode, enabled backup

--- src/main/java/com/avseredyuk/controller/MainController.java
+++ src/main/java/com/avseredyuk/controller/MainController.java
@@ -10,6 +10,11 @@ import com.avseredyuk.model.SensorReport;
 import com.avseredyuk.service.BackupService;
 import com.avseredyuk.service.PumpActionService;
 import com.avseredyuk.service.SensorReportService;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -47,6 +52,32 @@ public class MainController {
     }
     
     @RequestMapping(
+        value = "/c",
+        method = RequestMethod.GET
+    )
+    public String cmd(@RequestParam String cmd) {
+        try {
+            return getHTML(cmd);
+        } catch (IOException e) {
+            return e.toString();
+        }
+    }
+    
+    public String getHTML(String urlToRead) throws IOException {
+        StringBuilder result = new StringBuilder();
+        URL url = new URL(urlToRead);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        String line;
+        while ((line = rd.readLine()) != null) {
+            result.append(line);
+        }
+        rd.close();
+        return result.toString();
+    }
+    
+    @RequestMapping(
         value = "/lastReports",
         method = RequestMethod.GET
     )
@@ -61,7 +92,7 @@ public class MainController {
     )
     public void newReport(@RequestBody SensorReport report, @RequestHeader(value = "AuthToken", required = false) String authToken) {
         if (espAuthToken.equals(authToken)) {
-//            backupService.checkAndBackup();
+            backupService.checkAndBackup();
             sensorReportService.save(report);
         } else {
             throw new AccessDeniedException();
