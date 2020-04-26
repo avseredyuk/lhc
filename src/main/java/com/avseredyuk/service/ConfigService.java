3ea172e04ae024b61107c1fbfad7023310b3b659
xurset <a.v.seredyuk@gmail.com>
2020-04-26 11:44:30 +0300

    fixed #106 add settings validation

--- src/main/java/com/avseredyuk/service/ConfigService.java
+++ src/main/java/com/avseredyuk/service/ConfigService.java
@@ -5,6 +5,8 @@ import com.avseredyuk.model.Config;
 import com.avseredyuk.model.Config.ConfigKey;
 import com.avseredyuk.repository.ConfigRepository;
 import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -29,6 +31,12 @@ public class ConfigService {
     }
 
     public Config saveOrThrow(Config config) {
+        if (StringUtils.isBlank(config.getKey())) {
+            throw new InconsistentDataException("Empty key");
+        }
+        if (StringUtils.isBlank(config.getValue())) {
+            throw new InconsistentDataException("Empty value");
+        }
         if (configRepository.findByKey(config.getKey()).isPresent()) {
             throw new InconsistentDataException("Already existing key");
         }
